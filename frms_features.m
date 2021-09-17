413f6f0bc0f97b32fb7c67937563cbf53ffe1784
vasanza <vasanza@gmail.com>
2021-09-17 16:27:37 -0500

    Add files via upload

--- frms_features.m
+++ frms_features.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 % Reshape Array using rms features by time windows
 % input: Input Data (DataIn)
 % input: Time window or timestep value (TimeStep)
@@ -14,21 +13,4 @@ function [DataRMS] = frms_features(DataIn,TimeStep)
         end
         DataRMS=[DataRMS Feature];
     end
-=======
-% Reshape Array using rms features by time windows
-% input: Input Data (DataIn)
-% input: Time window or timestep value (TimeStep)
-% return: Reshape Array with rms features (DataRMS)
-% More examples: https://github.com/vasanza/Matlab_Code
-% Read more: https://vasanza.blogspot.com/
-function [DataRMS] = frms_features(DataIn,TimeStep)
-    DataRMS=[];
-    for i = 1:size(DataIn,2)%columnas
-        Feature=[];
-        for j = 1:TimeStep:size(DataIn,1)-TimeStep%filas
-            Feature=[Feature; rms(DataIn(j:j+TimeStep,i))];
-        end
-        DataRMS=[DataRMS Feature];
-    end
->>>>>>> 81621d7ab1e99236a004fd14fbcbc9e18898f652
 end
