3d5476506e1ba02a2aa95601492ad9c662afa304
vasanza <vasanza@gmail.com>
2022-11-27 08:03:45 -0500

    Add files via upload

---
+++ mean_features.m
@@ -0,0 +1,13 @@
+% Reshape Array using mean features by time windows
+% input: Input Data (DataIn)
+% input: Time window or timestep value (TimeStep)
+% return: Reshape Array with mean features (DataMean)
+% More examples: https://github.com/vasanza/Matlab_Code
+% Read more: https://vasanza.blogspot.com/
+function [DataMean] = mean_features(DataIn,TimeStep)
+    DataMean=[];
+        for i = 1:TimeStep:length(DataIn)-TimeStep
+            DataMean=[DataMean; mean(DataIn(i:i+TimeStep,:))];
+        end
+        
+end
